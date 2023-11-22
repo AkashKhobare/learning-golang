f97c89317bca606a7b62ed55ed599d2527f9ba07
Akash Khobare <35064095+AkashKhobare@users.noreply.github.com>
2023-11-22 15:40:59 +0000

    Added changes to updated struct fields

--- challenges/composite-types/main.go
+++ challenges/composite-types/main.go
@@ -6,11 +6,24 @@ type user struct {
 	username, email string
 }
 
+func (user *user) updateEmail(email string) {
+	user.email = email
+}
+
+func (user user) printUserDetails() {
+	fmt.Printf("\nusername: %v, email: %v\n", user.username, user.email)
+}
+
 func main() {
 	user := user{
 		username: "akashk",
 		email:    "akashk@gmail.com",
 	}
+	user.printUserDetails()
 
-	fmt.Printf("username: %v, email: %v\n", user.username, user.email)
+	// update email of user
+	user.updateEmail("akashkv2@gmail.com")
+	user.printUserDetails()
 }
+
+
