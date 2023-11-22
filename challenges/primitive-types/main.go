276062fff3cbcc1ce1c1a5bdf6762e5fbe6a41a5
Akash Khobare <35064095+AkashKhobare@users.noreply.github.com>
2023-11-22 15:27:03 +0000

    Learnt primitive types in golang

---
+++ challenges/primitive-types/main.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+// package level vars
+var val = "global var"
+
+func main() {
+	// local var
+	val := 250
+	fmt.Printf("Local val: %v\n", val)
+
+	// get global var
+	fmt.Printf("Global val: %v\n", getGlobalVal())
+
+	// create pointer to local var
+	var pVal *int = &val
+
+	// print address of local var
+	fmt.Printf("Local val address: %v\n", pVal)
+
+	// update local var value using pointer
+	*pVal = 350
+
+	// print updated local var
+	fmt.Printf("Updated local val: %v\n", val)
+}
+
+func getGlobalVal() string {
+	return val
+}
